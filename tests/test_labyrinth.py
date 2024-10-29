d1e3584b4c1797261f4673d9910a0fd506dc9e0a
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-10-29 08:52:22 -0500

    feat: add tests

---
+++ tests/test_labyrinth.py
@@ -0,0 +1,24 @@
+import pytest
+
+import imageio
+
+from gym_multigrid.envs.labyrinth import LabyrinthEnv
+
+
+def test_labyrinth() -> None:
+
+    animation_path: str = "tests/out/animations/labyrinth.gif"
+
+    env = LabyrinthEnv()
+
+    obs, _ = env.reset()
+    frames = [env.render()]
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        frames.append(env.render())
+        if terminated or truncated:
+            break
+
+    imageio.mimsave(animation_path, frames, duration=0.5, loop=10)
