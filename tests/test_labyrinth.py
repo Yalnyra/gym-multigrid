fab436cedda0c6c2b37a0c94bf7495cf1f1a472e
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-10-29 11:53:35 -0500

    test: add door open checker

--- tests/test_labyrinth.py
+++ tests/test_labyrinth.py
@@ -25,3 +25,26 @@ def test_labyrinth() -> None:
             break
 
     imageio.mimsave(animation_path, frames, loop=10)
+
+
+def test_check_door_0() -> None:
+
+    animation_path: str = "tests/out/animations/labyrinth_door_0.gif"
+
+    init_pos: tuple[tuple[int, int], ...] = ((3, 1), (3, 2), (3, 3))
+
+    env = LabyrinthEnv(init_pos=init_pos)
+    action = [env.actions.right, env.actions.right, env.actions.right]
+    obs, _ = env.reset()
+    frames = [env.render()]
+
+    for i in range(3):
+        obs, reward, terminated, truncated, info = env.step(action)
+        frames.append(env.render())
+        print(f"reward: {reward}")
+        print(f"terminated: {terminated}")
+        print(f"truncated: {truncated}")
+        if terminated or truncated:
+            break
+
+    imageio.mimsave(animation_path, frames, loop=10)
