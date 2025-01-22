28f520d0b496368c555f7047b11e81f6465c31c5
Yalnyra <vinokur.eg@gmail.com>
2025-01-22 13:41:47 +0200

    feat: random policy class

---
+++ pz_multigrid/policy/wildfire/heuristic.py
@@ -0,0 +1,32 @@
+from typing import TypedDict
+
+import numpy as np
+
+from gym_multigrid.core.agent import ActionsT
+from gym_multigrid.policy.base import BaseAgentPolicy
+
+
+class RwPolicy(BaseAgentPolicy):
+    """
+    Random walk policy
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "rw"
+
+    def act(self, observation: TypedDict, actions: ActionsT) -> int:
+        return np.random.choice(list(actions))
+    
+
+class AdjFirePolicy(BaseAgentPolicy):
+    """
+    Random adjacent fire policy
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "adjfire"
+
+    def act(self, observation: TypedDict, actions: ActionsT) -> int:
+        return np.random.choice(list(actions))
